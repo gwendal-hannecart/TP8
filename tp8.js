9aa5477af674a609dd68e0e78a8485a8cd2d03b7
gwendal-hannecart <gwendal.hannecart@gmail.com>
2020-05-21 21:03:34 +0200

    commit

--- tp8.js
+++ tp8.js
@@ -6,13 +6,22 @@
   function changeButtonColor(MouseEvent){
     let buton=document.getElementById("change");
     buton.style.backgroundColor="rgb("+Math.floor((Math.random() * 255) )+","+Math.floor((Math.random() * 255) )+","+Math.floor((Math.random() * 255) )+") ";
-    console.log(" Clients coords :("+MouseEvent.clientX+"),"+MouseEvent.clientX+")");
   
   }
 
+ 
+function printcoord(coord){
+   change.innerText = `cordonnees ( X ; Y ): (${coord.clientX} ; ${coord.clientY})`;
+}
+ 
+
 function main(){
     let okButton = document.getElementById("change");
+    changeButtonColor();
     okButton.addEventListener("mouseover", changeButtonColor);
+    okButton.addEventListener("mouseover",printcoord);
+ 
+   
    
 }
 main();
